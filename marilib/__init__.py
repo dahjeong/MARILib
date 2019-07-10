1f1593513d716501dd0e13e56031f0a0ab974d6e
Thales Delmiro <thales.delmiro@irt-saintexupery.com>
2019-07-10 12:28:28 +0200

    Embed numpy or autograd.numpy (if using autograd) in marilib

--- marilib/__init__.py
+++ marilib/__init__.py
@@ -3,6 +3,17 @@
 """
 Created on Tue Sep 25 16:22:36 2018
 
-@author: Nicolas Peteilh
+@author: Nicolas Peteilh, François Gallard, Thales Delmiro
 """
+import numpy as __np
+import marilib
+import sys
 
+sys.modules["marilib"].numpy = __np
+sys.modules["marilib.numpy"] = __np
+
+
+def use_autograd():
+    from autograd import numpy as __agnpy
+    sys.modules["marilib"].numpy = __agnpy
+    sys.modules["marilib.numpy"] = __agnpy
