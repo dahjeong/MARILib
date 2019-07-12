170f152ca37f9b8e88d586bf25175e736d5c4892
Thales Delmiro <thales.delmiro@irt-saintexupery.com>
2019-07-12 10:02:53 +0200

    Add fsolve compatibility with autograd

--- marilib/__init__.py
+++ marilib/__init__.py
@@ -18,3 +18,29 @@ def use_autograd():
     from autograd import numpy as __agnpy
     sys.modules["marilib"].numpy = __agnpy
     sys.modules["marilib.numpy"] = __agnpy
+    marilib.is_using_autograd = True
+    from autograd import jacobian
+    from autograd.numpy import array
+    from numpy.linalg import solve as _np_solve
+    from numpy import dot as _np_dot
+    from scipy.optimize import fsolve as _scip_fsolve
+    from autograd.extend import primitive, defvjp
+
+    @primitive
+    def fsolve(res, y, x, args=()):
+        args = (x,) + args
+        fprime = jacobian(res, argnum=0)
+        return _scip_fsolve(res, y, x, fprime=fprime)
+
+    @primitive
+    def fsolve_vjp(g, ans, res, y0, x, args=()):
+        drdy = jacobian(res, argnum=0)(ans, x, args)
+        drdx = jacobian(res, argnum=1)(ans, x, args)
+        dr_dx_g = _np_dot(drdx, g)
+        dy_dx_dot = -_np_solve(drdy, dr_dx_g)
+        return dy_dx_dot
+
+    defvjp(fsolve, lambda ans, res, y0, x, args: lambda g: fsolve_vjp(g, ans, res, y0, x, args),
+           argnums=[2])
+
+    __agnpy.fsolve = fsolve
